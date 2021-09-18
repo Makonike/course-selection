d59ed74bf28321e1464bc722736d57c955bf3750
Makonike <1694585033@qq.com>
2021-09-18 14:44:50 +0800

    feat(util.dpool): 分离自动注入的方法，添加@Controller注解扫描

--- src/main/java/com/zxd/www/util/ioc/factory/BeanFactory.java
+++ src/main/java/com/zxd/www/util/ioc/factory/BeanFactory.java
@@ -3,6 +3,7 @@ package com.zxd.www.util.ioc.factory;
 import com.zxd.www.util.ioc.annotation.Autowired;
 import com.zxd.www.util.ioc.annotation.Component;
 import com.zxd.www.util.ioc.scan.ClassScanner;
+import com.zxd.www.util.mvc.annotation.Controller;
 
 import java.lang.reflect.Field;
 import java.util.*;
@@ -17,16 +18,36 @@ public class BeanFactory {
 
     private static final Map<Class<?>,Object> CLASSES_INSTANCE = new HashMap<>();
 
+    private static BeanFactory instance = null;
+
+    private BeanFactory(String path) {
+        ClassScanner.scanClasses(path);
+        instanceBeans();
+        autowiredBean();
+    }
+
+    public static void initBeanFactory(String path){
+        if(instance == null){
+            instance = new BeanFactory(path);
+        }
+    }
+
+
+
     public static Object getBean(Class<?> clazz){
-        return CLASSES_INSTANCE.get(clazz);
+        if(CLASSES_INSTANCE.containsKey(clazz)){
+            return CLASSES_INSTANCE.get(clazz);
+        }
+        throw new RuntimeException("can't find the class: " + clazz.getName());
     }
 
     public static void instanceBeans(){
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         List<String> cfn = ClassScanner.getClassFullName();
         if(cfn.size() > 0){
             for (String className : cfn) {
                 try {
-                    createBean(Class.forName(className));
+                    createBean(classLoader.loadClass(className));
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                     throw new RuntimeException(e);
@@ -35,9 +56,13 @@ public class BeanFactory {
         }
     }
 
+    /**
+     * 创建bean对象
+     * @param clazz clazz
+     */
     private static void createBean(Class<?> clazz){
-        // 过滤掉不带Component注解的类
-        if (!clazz.isAnnotationPresent(Component.class)) {
+        // 过滤掉不带Component和Controller注解的类
+        if (!clazz.isAnnotationPresent(Component.class) && !clazz.isAnnotationPresent(Controller.class)) {
             return;
         }
         Object classInstance = null;
@@ -47,35 +72,45 @@ public class BeanFactory {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (Field field : clazz.getDeclaredFields()) {
-            // 查询需要自动注入的字段属性
-            if (!field.isAnnotationPresent(Autowired.class)) {
-                continue;
-            }
-            // 获取字段类型
-            Class<?> fieldType = field.getType();
-            // 开启暴力反射
-            field.setAccessible(true);
-            if (fieldType.isInterface()) {
-                // 如果依赖的类型是接口，则查询其实现类,
-                // class1.isAssignableFrom(class2) = true; class1 可以从 class2 赋值，代表class2是class1类型，可分配class2对象给class1
-                for (Class<?> key : BeanFactory.CLASSES_INSTANCE.keySet()) {
-                    if (fieldType.isAssignableFrom(key)) {
-                        fieldType = key;
-                        break;
-                    }
-                }
-            }
-            try {
-                // 注入实例
-                field.set(classInstance, BeanFactory.getBean(fieldType));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-        }
         // 将该类放入容器
         CLASSES_INSTANCE.put(clazz, classInstance);
+    }
+
+    /**
+     * 为属性注入bean, 避免注入时找不到还没放入容器的类
+     */
+    private static void autowiredBean(){
+
+        for (Class<?> clazz : CLASSES_INSTANCE.keySet()) {
+            for (Field field : clazz.getDeclaredFields()) {
+                // 查询需要自动注入的字段属性
+                if (!field.isAnnotationPresent(Autowired.class)) {
+                    continue;
+                }
+                // 获取字段类型
+                Class<?> fieldType = field.getType();
+                // 开启暴力反射
+                field.setAccessible(true);
+                if (fieldType.isInterface()) {
+                    // 如果依赖的类型是接口，则查询其实现类,
+                    for (Class<?> key : BeanFactory.CLASSES_INSTANCE.keySet()) {
+                        if (fieldType.isAssignableFrom(key)) {
+                            fieldType = key;
+                            break;
+                        }
+                    }
+                }
+
+                try {
+                    // 注入实例
+                    field.set(CLASSES_INSTANCE.get(clazz), BeanFactory.getBean(fieldType));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
     }
 
     /**
