3a0574d1ace700a3e9a0fa38017937675a507995
Makonike <1694585033@qq.com>
2021-09-24 17:02:25 +0800

    feat(controller): 增加课程的增删改查功能

--- src/main/java/com/zxd/www/controller/CourseController.java
+++ src/main/java/com/zxd/www/controller/CourseController.java
@@ -5,8 +5,10 @@ import com.zxd.www.po.Course;
 import com.zxd.www.service.CourseService;
 import com.zxd.www.util.ioc.annotation.Autowired;
 import com.zxd.www.util.mvc.annotation.Controller;
+import com.zxd.www.util.mvc.annotation.RequestBody;
 import com.zxd.www.util.mvc.annotation.RequestMapping;
 import com.zxd.www.util.mvc.annotation.RequestParam;
+import com.zxd.www.util.mvc.constant.RequestMethodConstant;
 
 import java.util.List;
 
@@ -46,8 +48,31 @@ public class CourseController {
         return new JsonResponse().notFound().message("未找到该课程");
     }
 
+    @RequestMapping(path = "/course/add", method = RequestMethodConstant.POST)
+    public JsonResponse addCourse(@RequestBody("course") Course course){
+        if(courseService.addCourse(course)){
+            return new JsonResponse();
+        }
+        return new JsonResponse().badRequest().message("请求错误，添加课程信息失败！");
+    }
 
 
+    @RequestMapping(path = "/course/update", method = RequestMethodConstant.PUT)
+    public JsonResponse updateCourse(@RequestBody("course") Course course){
+        if(courseService.updateCourse(course)){
+            return new JsonResponse();
+        }
+        return new JsonResponse().badRequest().message("请求错误，修改课程信息失败！");
 
+    }
+
+    @RequestMapping(path = "/course/delete", method = RequestMethodConstant.DELETE)
+    public JsonResponse deleteCourse(@RequestParam("courseId") Integer courseId){
+        if(courseService.deleteCourse(courseId)){
+            return new JsonResponse();
+        }
+        return new JsonResponse().badRequest().message("请求错误，删除课程信息失败！");
+
+    }
 
 }
